6896e8b216ede4533f929c9ca6d5ca8b1ef57880
Miguel Ángel MC <mamoca28@hotmail.es>
2024-03-12 13:17:56 +0100

    feat: Terminado Tutorial

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -32,7 +32,91 @@ Action ComportamientoJugador::think(Sensores sensores)
 	cout << "  Reset: " << sensores.reset;
 	cout << "  Vida: " << sensores.vida << endl<< endl;
 
+    // Fase de observación
+    switch (last_action)
+    {
+        case actWALK:
+            switch (current_state.brujula)
+            {
+                case norte: 
+                    current_state.fil--;
+                break;
+                case noreste:
+                    current_state.fil--;
+                    current_state.col++;
+                break;
+                case este:
+                    current_state.col++;
+                break;
+                case sureste:
+                    current_state.fil++;
+                    current_state.col++;
+                break;
+                case sur:
+                    current_state.fil++;
+                break;
+                case suroeste:
+                    current_state.fil++;
+                    current_state.col--;
+                break;
+                case oeste:
+                    current_state.col--;
+                break;
+                case noroeste:
+                    current_state.fil--;
+                    current_state.col--;
+                break;
+            }
+        break;
+        case actRUN:
+            // Implementar
+        break;
+        case actTURN_SR:
+            current_state.brujula = static_cast<Orientacion> ((current_state.brujula+1)%8);
+            // girar_derecha = (rand() % 2 == 0);
+        break;
+        case actTURN_L:
+            current_state.brujula = static_cast<Orientacion> ((current_state.brujula+6)%8);
+            // girar_derecha = (rand() % 2 == 0);
+        break;
+    }
+
+    if (sensores.terreno[0] == 'G' && !bien_situado)
+    {
+        current_state.fil = sensores.posF;
+        current_state.col = sensores.posC;
+        current_state.brujula = sensores.sentido;
+        bien_situado = true;
+    }
+
+    if (bien_situado)
+    {
+        PonerTerrenoEnMatriz(sensores.terreno, current_state, mapaResultado);
+    }
+
+    // Fase de decisión de la nueva acción
+    if ((sensores.terreno[2] == 'T' || sensores.terreno[2] == 'S' || sensores.terreno[2] == 'G') && sensores.agentes[2] == '_')
+    {
+        accion = actWALK;
+    }
+    else if (!girar_derecha)
+    {
+        accion = actTURN_L;
+        girar_derecha = (rand() % 2 == 0);
+    }
+    else
+    {
+        accion = actTURN_SR;
+        girar_derecha = (rand() % 2 == 0);
+    }
+    // Devuelve el valor de la acción
+    last_action = accion;
 	return accion;
+}
+
+void ComportamientoJugador::PonerTerrenoEnMatriz(const vector<unsigned char> & terreno, const state & st, vector<vector<unsigned char>> & m)
+{
+    m[st.fil][st.col] = terreno[0];
 }
 
 int ComportamientoJugador::interact(Action accion, int valor)
